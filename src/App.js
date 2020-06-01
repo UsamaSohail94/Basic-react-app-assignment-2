494462cdada58323515c920c6fb255b661f771dd
UsamaSohail94 <37038343+UsamaSohail94@users.noreply.github.com>
2020-06-02 02:33:02 +0500

    updated app.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
      <h1>My first React App for bootcamp 2020</h1>
      <h2>deployed on surge</h2>
+     <p>Hello from Usama Sohail</p>
     </div>
   );
 }
